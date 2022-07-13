453d9f8b5409be1832fc1f0290dad9ddd6dff906
Steven Pousty <steve.pousty@gmail.com>
2022-07-13 00:27:08 -0700

    initial import after initializer

---
+++ src/test/java/org/molw/ParameterSetter/ParameterSetterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.molw.ParameterSetter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParameterSetterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
