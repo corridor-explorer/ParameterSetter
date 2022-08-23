6c0f4af5f050f0a9c684e646886f3b6ad19b85a8
Steven Pousty <steve.pousty@gmail.com>
2022-08-23 15:40:56 -0700

    got the web part working Still need to add the base of the API path - probably at API

--- src/main/java/org/molw/api/testAPIImpl.java
+++ src/main/java/org/molw/api/testAPIImpl.java
@@ -3,12 +3,16 @@ import org.molw.generated.api.TestApi;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+@RestController
+@RequestMapping("/v1/test")
 public class testAPIImpl implements TestApi {
 
     @Override
     public ResponseEntity<String> getTestResponse(){
+
         return new ResponseEntity<>("The Test Worked", HttpStatus.OK);
     }
 }
